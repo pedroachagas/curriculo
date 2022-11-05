cb823e809f5319c09b77b393f8920c9734a27db1
Pedro Chagas <pchagas96@gmail.com>
2022-11-05 15:52:39 -0300

    info adicionada

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,9 @@ st.image(image, width=150)
 
 st.markdown('## Quem sou eu', unsafe_allow_html=True)
 st.info('''
-- Experienced Educator, Researcher and Administrator with almost twenty years of experience in data-oriented environment and a passion for delivering insights based on predictive modeling. 
-- Strong verbal and written communication skills as demonstrated by extensive participation as invited speaker at `10` conferences as well as publishing 149 research articles.
-- Strong track record in scholarly research with H-index of `32` and total citation of 3200+.
+- Duplo-graduado em Engenheria de Produção pela UFRJ e pela ENSAM Paristech (Bolsista Honorário BRAFITEC)
+- Apaixonado por Ciência de Dados e Machine Learning
+- Em constante busca de conhecimento :brain:
 ''')
 
 #####################
